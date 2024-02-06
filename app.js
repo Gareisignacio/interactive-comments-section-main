ef45b705bcbad05b6d18cff7f7df0befb62de60b
Gareisignacio <93623301+Gareisignacio@users.noreply.github.com>
2024-02-06 13:57:35 -0300

    script

--- app.js
+++ app.js
@@ -1,90 +1,91 @@
-const data ={
-   "currentUser": {
-     "image": { 
-       "png": "./images/avatars/image-juliusomo.png",
-       "webp": "./images/avatars/image-juliusomo.webp"
-     },
-     "username": "juliusomo"
-   },
-   "comments": [
-     {
-       "id": 1,
-       "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-       "createdAt": "1 month ago",
-       "score": 12,
-       "user": {
-         "image": { 
-           "png": "./images/avatars/image-amyrobson.png",
-           "webp": "./images/avatars/image-amyrobson.webp"
-         },
-         "username": "amyrobson"
-       },
-       "replies": []
-     },
-     {
-       "id": 2,
-       "content": "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-       "createdAt": "2 weeks ago",
-       "score": 5,
-       "user": {
-         "image": { 
-           "png": "./images/avatars/image-maxblagun.png",
-           "webp": "./images/avatars/image-maxblagun.webp"
-         },
-         "username": "maxblagun"
-       },
-       "replies": [
-         {
-           "id": 3,
-           "content": "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-           "createdAt": "1 week ago",
-           "score": 4,
-           "replyingTo": "maxblagun",
-           "user": {
-             "image": { 
-               "png": "./images/avatars/image-ramsesmiron.png",
-               "webp": "./images/avatars/image-ramsesmiron.webp"
-             },
-             "username": "ramsesmiron"
-           }
-         },
-         {
-           "id": 4,
-           "content": "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-           "createdAt": "2 days ago",
-           "score": 2,
-           "replyingTo": "ramsesmiron",
-           "user": {
-             "image": { 
-               "png": "./images/avatars/image-juliusomo.png",
-               "webp": "./images/avatars/image-juliusomo.webp"
-             },
-             "username": "juliusomo"
-           }
-         }
-       ]
-     }
-   ]
- };
+const data = {
+  currentUser: {
+    image: {
+      png: "./images/avatars/image-juliusomo.png",
+      webp: "./images/avatars/image-juliusomo.webp",
+    },
+    username: "juliusomo",
+  },
+  comments: [
+    {
+      id: 1,
+      content:
+        "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+      createdAt: "1 month ago",
+      score: 12,
+      user: {
+        image: {
+          png: "./images/avatars/image-amyrobson.png",
+          webp: "./images/avatars/image-amyrobson.webp",
+        },
+        username: "amyrobson",
+      },
+      replies: [],
+    },
+    {
+      id: 2,
+      content:
+        "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+      createdAt: "2 weeks ago",
+      score: 5,
+      user: {
+        image: {
+          png: "./images/avatars/image-maxblagun.png",
+          webp: "./images/avatars/image-maxblagun.webp",
+        },
+        username: "maxblagun",
+      },
+      replies: [
+        {
+          id: 3,
+          content:
+            "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+          createdAt: "1 week ago",
+          score: 4,
+          replyingTo: "maxblagun",
+          user: {
+            image: {
+              png: "./images/avatars/image-ramsesmiron.png",
+              webp: "./images/avatars/image-ramsesmiron.webp",
+            },
+            username: "ramsesmiron",
+          },
+        },
+        {
+          id: 4,
+          content:
+            "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+          createdAt: "2 days ago",
+          score: 2,
+          replyingTo: "ramsesmiron",
+          user: {
+            image: {
+              png: "./images/avatars/image-juliusomo.png",
+              webp: "./images/avatars/image-juliusomo.webp",
+            },
+            username: "juliusomo",
+          },
+        },
+      ],
+    },
+  ],
+};
 
-const container = document.querySelector('.container')
+const container = document.querySelector(".container");
 
-
-for(let i = 0; i < data.comments.length; i++){
+for (let i = 0; i < data.comments.length; i++) {
   let allComments = document.createElement("div");
   container.appendChild(allComments);
-  allComments.classList.add("all-comments")
+  allComments.classList.add("all-comments");
 
   let card = document.createElement("div");
   card.classList.add("card");
 
   let currentUser = data.currentUser.username;
 
-
-  if(data.comments[i].replies.length === 0){
+  if (data.comments[i].replies.length === 0) {
     allComments.appendChild(card);
-    card.innerHTML =
-  `
+    card.innerHTML = `
          <div class="score">
             <div class="plus-icon"></div>
             <p>${data.comments[i].score}</p>
@@ -110,20 +111,20 @@ for(let i = 0; i < data.comments.length; i++){
             </div>
          </div>
    `;
-  } else{
+  } else {
     allComments.appendChild(card);
 
-    for(let j = 0; j < data.comments[i].replies.length; j++){
+    for (let j = 0; j < data.comments[i].replies.length; j++) {
       let replyUser = data.comments[i].replies[j].user.username;
 
       let replies = document.createElement("div");
       replies.classList.add("card");
       replies.classList.add("replies");
 
-      card.innerHTML =`
+      card.innerHTML = `
           <div class="score">
             <div class="plus-icon"></div>
-            <p>${data.comments[i].score}</p>
+            <p class='score-num'>${data.comments[i].score}</p>
             <div class="minus-icon" ></div>
           </div>
           <div class="info">
@@ -146,12 +147,11 @@ for(let i = 0; i < data.comments.length; i++){
             </div>
          </div>
     `;
-      if(currentUser === replyUser){
-        replies.innerHTML =
-    `
+      if (currentUser === replyUser) {
+        replies.innerHTML = `
               <div class="score">
                 <div class="plus-icon"></div>
-                <p>${data.comments[i].replies[j].score}</p>
+                <p class='score-num'>${data.comments[i].replies[j].score}</p>
                 <div class="minus-icon"></div>
               </div>
               <div class="info">
@@ -185,12 +185,11 @@ for(let i = 0; i < data.comments.length; i++){
                   </div>
                 </div>
               </div>`;
-      } else{
-        replies.innerHTML =
-    `
+      } else {
+        replies.innerHTML = `
               <div class="score">
                 <div class="plus-icon"></div>
-                <p>${data.comments[i].replies[j].score}</p>
+                <p class='score-num'>${data.comments[i].replies[j].score}</p>
                 <div class="minus-icon"></div>
               </div>
               <div class="info">
@@ -222,11 +221,25 @@ for(let i = 0; i < data.comments.length; i++){
               </div>
     `;
       }
-      
+
       allComments.appendChild(replies);
+
+      // plusBtn.addEventListener('click', ()=>{
+      //   let scoreNum = document.querySelector('.score-num')
+      //   alert(scoreNum.textContent);
+      // })
     }
   }
 }
+const plusBtn = document.querySelectorAll(".plus-icon");
 
-let replyBtns = document.querySelectorAll('.reply');
-replyBtns.forEach
+plusBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let scoreNum = document.querySelector(".score-num");
+    alert();
+  });
+});
+
+// const plusBtn = document.querySelector('.plus-icon');
+// const minusBtn = document.querySelector('.minus-icon');
+// const replyBtn = document.querySelector('.reply-icon');
